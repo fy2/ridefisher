296a3a901ff3552c2e8174e36e013408383ecbd4
Feyruz Yalcin <feyruz.yalcin@net-a-porter.com>
2018-04-15 15:47:26 +0100

    persist logs separate

--- script/ra_persist.pl
+++ script/ra_persist.pl
@@ -6,7 +6,7 @@ use lib "$Bin/../lib";
 use Log::Log4perl;
 use RideAway::Persistence;
 
-Log::Log4perl::init_and_watch("$Bin/../config/log4perl.conf",10);
+Log::Log4perl::init_and_watch("$Bin/../config/log4perl_persist.conf",10);
 
 my $persist = RideAway::Persistence->new(config_file => "$Bin/../config/config.live.ini");
 $persist->run;
